ef3b2a4d888355334540d71f9833a58096329a2c
JamieLoLo <jamielo0320@gmail.com>
2022-12-11 14:04:16 +0800

    Nothing change (type style)

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import "./reset.module.scss";
-import "./base.module.scss";
-import "./UI/Button.module.scss";
-import "bootstrap/dist/css/bootstrap-grid.min.css";
-import UserLoginPage from "./pages/UserLoginPage";
-import SignUpPage from "./pages/SignUpPage";
-import AdminLoginPage from "./pages/AdminLoginPage";
-import SettingPage from "./pages/SettingPage";
+import './reset.module.scss'
+import './base.module.scss'
+import './UI/Button.module.scss'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import UserLoginPage from './pages/UserLoginPage'
+import SignUpPage from './pages/SignUpPage'
+import AdminLoginPage from './pages/AdminLoginPage'
+import SettingPage from './pages/SettingPage'
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path="users/login" element={<UserLoginPage />} />
-          <Route path="signup" element={<SignUpPage />} />
-          <Route path="admin/login" element={<AdminLoginPage />} />
-          <Route path="users/setting" element={<SettingPage />} />
+          <Route path='users/login' element={<UserLoginPage />} />
+          <Route path='signup' element={<SignUpPage />} />
+          <Route path='admin/login' element={<AdminLoginPage />} />
+          <Route path='users/setting' element={<SettingPage />} />
         </Routes>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
