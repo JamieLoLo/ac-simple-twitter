840419fb07c88c5691395c12993596c699ac14b7
JamieLoLo <jamielo0320@gmail.com>
2022-12-13 20:55:13 +0800

    Like api

--- src/api/likeApi.jsx
+++ src/api/likeApi.jsx
@@ -11,8 +11,26 @@ axiosInstance.interceptors.request.use((config) => {
   }
   return config
 })
-// 喜歡一則推文
-
+// 喜歡一則推文tweets/504/like
+export const likeApi = async (tweet_id) => {
+  try {
+    const res = await axiosInstance.post(`${likeURL}/${tweet_id}/like`)
+    return res
+  } catch (error) {
+    console.error(error)
+    console.error('[Like Failed]: ', error)
+    return error
+  }
+}
 
 // 取消喜歡一則推文
-
+export const unLikeApi = async (tweet_id) => {
+  try {
+    const res = await axiosInstance.post(`${likeURL}/${tweet_id}/unlike`)
+    return res
+  } catch (error) {
+    console.error(error)
+    console.error('[unLike Failed]: ', error)
+    return error
+  }
+}
