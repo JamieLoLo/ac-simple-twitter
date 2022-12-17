dd30f0798005760f1f1729e0372b2a53c0eb0181
Yu-Jia Huang <shiusun246@gmail.com>
2022-12-17 20:02:23 +0800

    fix

--- src/api/followshipsApi.jsx
+++ src/api/followshipsApi.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-const followshipsURL =
-  'https://fierce-plains-47262.herokuapp.com/api/followships'
+// const followshipsURL =
+//   'https://fierce-plains-47262.herokuapp.com/api/followships'
 // const followshipsURL =
 //   'https://dry-anchorage-06913.herokuapp.com/api/followships'
-// const followshipsURL =
-//   'https://e3ae-223-136-148-157.jp.ngrok.io/api/followships'
+const followshipsURL =
+  'https://e3ae-223-136-148-157.jp.ngrok.io/api/followships'
 
 const axiosInstance = axios.create({ baseURL: followshipsURL })
 
