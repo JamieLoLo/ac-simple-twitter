d277dda0216790ea45828fc58b836cc3e9d2178b
JamieLoLo <jamielo0320@gmail.com>
2022-12-18 00:19:29 +0800

    admin page lazy loading

--- src/pages/AdminAllUsersPage.jsx
+++ src/pages/AdminAllUsersPage.jsx
@@ -1,14 +1,18 @@
 import styles from './AdminAllUsersPage.module.scss'
-import { AdminGrid } from '../Layout/GridSystemWrapper'
+import { AdminUserPageGrid } from '../Layout/GridSystemWrapper'
 import { useLocation, useNavigate } from 'react-router-dom'
 import AdminUserItem from '../components/AdminUserItem'
 import { useState, useEffect } from 'react'
 import { adminGetAllUsersApi } from '../api/adminApi'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { ReactComponent as LoadingIcon } from '../components/assets/icons/loading.svg'
 
 const AdminAllUsersPage = () => {
   const navigate = useNavigate()
   const pathname = useLocation().pathname
   const [data, setData] = useState([])
+  const [page, setPage] = useState(1)
+  const [hasMore, setHasMore] = useState(true)
   const authToken = localStorage.getItem('authToken')
 
   useEffect(() => {
@@ -22,23 +26,54 @@ const AdminAllUsersPage = () => {
   ))
 
   // admin 獲得所有 user
+
+  const adminGetAllUsers = async () => {
+    try {
+      const res = await adminGetAllUsersApi(1)
+      setData(res.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   useEffect(() => {
-    const adminGetAllTweets = async () => {
+    adminGetAllUsers(1)
+    setPage(2)
+  }, [navigate])
+
+  // lazy loading for user list
+  const changePage = () => {
+    const adminGetAllUsers = async () => {
       try {
-        const res = await adminGetAllUsersApi()
-        setData(res.data)
+        const res = await adminGetAllUsersApi(page)
+        setHasMore(res.data.length)
+        await setData(data.concat(res.data))
+        setPage((page) => page + 1)
       } catch (error) {
         console.error(error)
       }
     }
-    adminGetAllTweets()
-  }, [navigate])
+    adminGetAllUsers()
+  }
 
   return (
-    <AdminGrid pathname={pathname}>
+    <AdminUserPageGrid pathname={pathname} id={'user__list'}>
       <div className={styles.title}>使用者列表</div>
-      <div className={styles.user__card__list}>{adminUserItemHelper}</div>
-    </AdminGrid>
+      <div className={styles.user__card__list}>
+        <InfiniteScroll
+          className={styles.infinite__scroll}
+          dataLength={data.length}
+          next={changePage}
+          hasMore={hasMore !== 0}
+          endMessage={null}
+          scrollableTarget='user__list'
+          loader={<LoadingIcon className={styles.loading__icon} />}
+          height={850}
+        >
+          {adminUserItemHelper}
+        </InfiniteScroll>
+      </div>
+    </AdminUserPageGrid>
   )
 }
 
