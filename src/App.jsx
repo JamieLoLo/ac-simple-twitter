9be425df776c012feaee235789658244400ad565
JamieLoLo <jamielo0320@gmail.com>
2022-12-19 17:38:41 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ const basename = process.env.PUBLIC_URL
 const App = () => {
   return (
     <div className='App' basename={basename}>
+      homepage
       <BrowserRouter>
         <Routes>
           <Route path='signup' element={<SignUpPage />} />
